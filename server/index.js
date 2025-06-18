02852f98a151b1b1f041107928b4564c78d2712d
harshit <harshitkumarsingh203@gmail.com>
2025-06-18 14:42:30 +0530

    update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://todo-app-client-omega-pied.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
